05ae31bec1b2ffd5bc14d5038401cd7574162b77
Areesh <areesh.192003@gmail.com>
2023-10-29 14:55:25 +0500

    Friendship ended with Ckeditor:(
    QuillJs is my new best friend <3

---
+++ UserInterface/wwwroot/BlazorQuill.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    window.QuillFunctions = {
+        createQuill: function (quillElement) {
+            var options = {
+                debug: 'info',
+                modules: {
+                    toolbar: '#toolbar'
+                },
+                placeholder: 'Compose an epic...',
+                readOnly: false,
+                theme: 'snow'
+            };
+            // set quill at the object we can call
+            // methods on later
+            new Quill(quillElement, options);
+        }
+    };
+})();
