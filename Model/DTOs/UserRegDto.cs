5116c43ef08709eddd1d99ad1df9093d8313c9d7
Areesh <areesh.192003@gmail.com>
2023-10-29 19:18:17 +0500

    working on implementing Logic in register

--- Model/DTOs/UserRegDto.cs
+++ Model/DTOs/UserRegDto.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 namespace Model.DTOs;
 
 public class UserRegDto
 {
+    [Required(ErrorMessage ="Username is required")]
     public string Username { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email is required")]
     public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password is required")]
     public string Password { get; set; } = string.Empty;
+    [Required(ErrorMessage = "FirstName is required")]
     public string FirstName { get; set; } = string.Empty;
+    [Required(ErrorMessage = "LastName is required")]
     public string LastName { get; set; } = string.Empty;
 }
